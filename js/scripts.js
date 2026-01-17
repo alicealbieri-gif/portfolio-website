a173ca1da18d9af609282099e1e3f18b21cb4720
Alice Albieri <alicealbieri@gmail.com>
2026-01-17 18:12:31 +0100

    apply all the changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,9 @@
-console.log("Hello! Welcome to Anima Creativa Studio.");
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   
-  // Check if the class is strictly 'navtoggle'
   if (x.className === 'navtoggle') {
-    // Add the 'responsive' class (notice the space before the word)
     x.className += ' responsive';
   } else {
-    // Remove the 'responsive' class
     x.className = 'navtoggle';
   }
 }
-// Smooth scrolling for anchor links
-$('a[href*="#"]').on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate(
-    {
-      scrollTop: $($(this).attr('href')).offset().top,
-    },
-    500,
-    'linear'
-  );
-});
